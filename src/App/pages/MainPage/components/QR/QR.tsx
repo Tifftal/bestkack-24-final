5eac7ef70e9407185cc592867b9eebe07ff3745a
DeDeimos <rodion.tolkachev.03@inbox.ru>
2024-04-28 02:41:58 +0300

    deimos: qr code

--- src/App/pages/MainPage/components/QR/QR.tsx
+++ src/App/pages/MainPage/components/QR/QR.tsx
@@ -1,9 +1,16 @@
 import styles from './QR.module.scss';
+import QRCode from 'react-qr-code';
+import { Text } from '@mantine/core';
 
 const QR = () => {
-    return (
-        <>QR</>
-    )
-}
+  return (
+    <div className={styles.qr}>
+      <div className={styles.qr__content}>
+        <QRCode value="RT5" />
+        <Text size="lg">Наша команда</Text>
+      </div>
+    </div>
+  );
+};
 
 export default QR;
